afa24e47c9ccee931852777d5d1f5bdcc76a235b
miguel <miguelangelcu@ufps.edu.co>
2018-12-27 12:03:01 -0500

    Added common functions to consulte sql and session state

---
+++ airQ/App_Code/onmotica.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace airQ.App_Code
+{
+    public class onmotica
+    {
+        public static void isLogged(System.Web.SessionState.HttpSessionState Session, HttpResponse Response)
+        {
+            if (Session["UsrID"] != null & Session["UsrName"] != null)
+            {
+                if (Convert.ToInt32(Session["UsrID"]) > 0 & Session["UsrName"].ToString() != "")
+                {
+                    Response.Redirect("/dashboard");
+                }
+            }
+        }
+        public static SqlDataReader fetchReader(string query)
+        {
+            SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["AirQConnectionString"].ConnectionString);
+            SqlCommand myCmd = new SqlCommand(query, myConn);
+            myConn.Open();
+            return myCmd.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+
+        public static DataSet fetchData(string query)
+        {
+            SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["AirQConnectionString"].ConnectionString);
+            SqlDataAdapter myAdapter = new SqlDataAdapter(query, myConn);
+            DataSet myData = new DataSet();
+            myAdapter.Fill(myData);
+            return myData;
+        }
+
+        public static object fetchScalar(string query)
+        {
+            SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["AirQConnectionString"].ConnectionString);
+            SqlCommand myCmd = new SqlCommand(query, myConn);
+            myConn.Open();
+            object scalar = myCmd.ExecuteScalar();
+            myConn.Close();
+            return scalar;
+        }
+
+        public static object fetchScalar(string query, int timeToWait)
+        {
+            SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["AirQConnectionString"].ConnectionString);
+            SqlCommand myCmd = new SqlCommand(query, myConn);
+            myConn.Open();
+            myCmd.CommandTimeout = timeToWait;
+            object scalar = myCmd.ExecuteScalar();
+            myConn.Close();
+            return scalar;
+        }
+
+        public static SqlDataReader fetchReader(SqlCommand myCmd)
+        {
+            SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["AirQConnectionString"].ConnectionString);
+            myCmd.Connection = myConn;
+            myConn.Open();
+            return myCmd.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+
+        public static DataSet fetchData(SqlCommand myCmd)
+        {
+            SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["AirQConnectionString"].ConnectionString);
+            myCmd.Connection = myConn;
+            SqlDataAdapter myAdapter = new SqlDataAdapter(myCmd);
+            DataSet myData = new DataSet();
+            myAdapter.Fill(myData);
+            return myData;
+        }
+
+        public static object fetchScalar(SqlCommand myCmd)
+        {
+            SqlConnection myConn = new SqlConnection(ConfigurationManager.ConnectionStrings["AirQConnectionString"].ConnectionString);
+            myCmd.Connection = myConn;
+            myConn.Open();
+            myCmd.CommandTimeout = 32400; //32400 sec = 9 hours
+            object scalar = myCmd.ExecuteScalar();
+            myConn.Close();
+            return scalar;
+        }
+    }    
+}
