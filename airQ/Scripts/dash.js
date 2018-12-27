e19053d858b0f114cbd8e0143a717ff33a04c17c
miguel5612 <miguelangelcu@ufps.edu.co>
2018-12-27 16:18:08 -0500

    Gauges grid OK

---
+++ airQ/Scripts/dash.js
@@ -0,0 +1,136 @@
+﻿google.charts.load('current', { 'packages': ['gauge'] });
+google.charts.setOnLoadCallback(drawCharts);
+
+function drawCharts() {
+    chart1();
+    chart2();
+    chart3();
+    chart4();
+    chart5();
+
+}
+function chart1() {
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Temperatura', 80],
+        ['Humedad', 55]
+    ]);
+
+    var options = {
+        width: 400, height: 120,
+        redFrom: 90, redTo: 100,
+        yellowFrom: 75, yellowTo: 90,
+        minorTicks: 5
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart-divTemperaturaHumedad'));
+
+    chart.draw(data, options);
+
+    setInterval(function () {
+        data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+    }, 13000);
+    setInterval(function () {
+        data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+    }, 5000);
+}
+function chart2() {
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Presion atmosferica', 80]
+    ]);
+
+    var options = {
+        width: 400, height: 120,
+        redFrom: 90, redTo: 100,
+        yellowFrom: 75, yellowTo: 90,
+        minorTicks: 5
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart-divPresionAtmosferica'));
+
+    chart.draw(data, options);
+
+    setInterval(function () {
+        data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+    }, 13000);
+}
+function chart3() {
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Alcoholes', 80]
+    ]);
+
+    var options = {
+        width: 400, height: 120,
+        redFrom: 90, redTo: 100,
+        yellowFrom: 75, yellowTo: 90,
+        minorTicks: 5
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart-divAlcoholes'));
+
+    chart.draw(data, options);
+
+    setInterval(function () {
+        data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+    }, 13000);
+}
+function chart4() {
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['TVOC', 80],
+        ['CO2', 55]
+    ]);
+
+    var options = {
+        width: 400, height: 120,
+        redFrom: 90, redTo: 100,
+        yellowFrom: 75, yellowTo: 90,
+        minorTicks: 5
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart-divTVOCCO2'));
+
+    chart.draw(data, options);
+
+    setInterval(function () {
+        data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+    }, 13000);
+    setInterval(function () {
+        data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+    }, 5000);
+}
+function chart5() {
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Gas Metano', 80]
+    ]);
+
+    var options = {
+        width: 400, height: 120,
+        redFrom: 90, redTo: 100,
+        yellowFrom: 75, yellowTo: 90,
+        minorTicks: 5
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart-divGasMetano'));
+
+    chart.draw(data, options);
+
+    setInterval(function () {
+        data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+    }, 13000);
+    setInterval(function () {
+        data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+        chart.draw(data, options);
+    }, 5000);
+}
+
--- airQ/dashboard.aspx
+++ airQ/dashboard.aspx
@@ -1,5 +1,56 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/dashboard.Master" AutoEventWireup="true" CodeBehind="dashboard.aspx.cs" Inherits="airQ.dashboard1" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script src="Scripts/dash.js"></script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+        <div class="col-sm-4 py-2">
+            <div class="card card-body h-100">
+                <h3>Temperatura/Humedad</h3>
+                <div id="chart-divTemperaturaHumedad" style="width: 400px; height: 120px;"></div>
+                    <a href="#" class="btn btn-outline-light">Outline</a>
+            </div>
+        </div>
+        <div class="col-sm-4 py-2">
+            <div class="card h-100 text-white bg-danger">
+                <div class="card-body">
+                    <h3>Presion atmosferica</h3>
+                    <div id="chart-divPresionAtmosferica" style="width: 400px; height: 120px;"></div>
+                    <a href="#" class="btn btn-outline-light">Outline</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 py-2">
+            <div class="card h-100 border-primary">
+                <div class="card-body">
+                    <h3>Alcoholes</h3>
+                    <div id="chart-divAlcoholes" style="width: 400px; height: 120px;"></div>
+                    <a href="#" class="btn btn-outline-light">Outline</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 py-2">
+            <div class="card h-100 card-body">
+               <h3>TVOC/CO2</h3>
+                    <div id="chart-divTVOCCO2" style="width: 400px; height: 120px;"></div>
+                    <a href="#" class="btn btn-outline-light">Outline</a>
+            </div>
+        </div>        
+        <div class="col-sm-4 py-2">
+            <div class="card h-100 card-body">
+               <h3>Gas Metano</h3>
+                    <div id="chart-divGasMetano" style="width: 400px; height: 120px;"></div>
+                    <a href="#" class="btn btn-outline-light">Outline</a>
+            </div>
+        </div>
+        <div class="col-sm-4 py-2">
+            <div class="card text-white bg-primary">
+                <div class="card-body">
+                     <h3>Ubicacion de la estacion</h3>
+                    <div id="chart-location" style="width: 400px; height: 120px;"></div>
+                    <a href="#" class="btn btn-outline-light">Outline</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
