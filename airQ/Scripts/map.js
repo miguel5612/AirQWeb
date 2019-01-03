9558d3da98e1367981c4bc9a9d6ba47b4311170d
miguel <miguelangelcu@ufps.edu.co>
2019-01-03 12:23:42 -0500

    Map added

---
+++ airQ/Scripts/map.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    //mapboxgl.accessToken = 'pk.eyJ1IjoibWlndWVsNTYxMiIsImEiOiJjanFndWVpdWMxdmtuNDJwdGxpMDkzdXo0In0.0IvcSGt-dMoJvLpW7XOkxw';
+    /*
+    var map = new mapboxgl.Map({
+        container: 'map',
+        zoom: 13,
+        center: [7.8958656, -72.4989307],
+        style: 'mapbox://styles/mapbox/streets-v9',
+        hash: true,
+        transformRequest: (url, resourceType) => {
+            if (resourceType === 'Source' && url.startsWith('http://myHost')) {
+                return {
+                    url: url.replace('http', 'https'),
+                    headers: { 'my-custom-header': true },
+                    credentials: 'include'  // Include cookies for cross-origin requests
+                }
+            }
+        }
+    });
+    var marker = mapboxgl.marker([51.5, -0.09]).addTo(mymap);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+    */
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoibWlndWVsNTYxMiIsImEiOiJjanFndWVpdWMxdmtuNDJwdGxpMDkzdXo0In0.0IvcSGt-dMoJvLpW7XOkxw'
+    }).addTo(map);
+
+    L.marker([7.8958656, -72.4989307]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+});
---
+++ airQ/map.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/dashboard.Master" AutoEventWireup="true" CodeBehind="map.aspx.cs" Inherits="airQ.map" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css"
+   integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+   crossorigin=""/>
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+ <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"
+   integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg=="
+   crossorigin=""></script>
+    <link href="Content/map.css" rel="stylesheet" />
+    <script src="Scripts/map.js"></script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div id="map"></div>
+</asp:Content>
