d4296c9555e6f651659cc6b32312921241112dcc
miguel <miguelangelcu@ufps.edu.co>
2019-02-27 11:42:59 -0500

    Testing mail send smtp

---
+++ airQ/Test/TestMail.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+
+namespace airQ.Test
+{
+    public partial class TestMail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage("onmotica@gmail.com", "miguelangelcu@ufps.edu.co");
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential("onmotica@gmail.com", "AbCd1234");
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            mail.Subject = "this is a test email.";
+            mail.Body = "this is my test email body";
+            client.Send(mail);
+        }
+    }
+}
